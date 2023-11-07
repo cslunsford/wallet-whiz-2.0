b43b11ab2c5c855eb71c5b3818de78168d140ebc
cslunsford <christopher.lunsford@uconn.edu>
2023-11-06 20:08:29 -0500

    added authorization middleware

---
+++ server/utils/auth.js
@@ -0,0 +1,39 @@
+const { GraphQLError } = require('graphql');
+const jwt = require('jsonwebtoken');
+
+const secret = 'WalletWhizSecret'
+const expiration = '2h';
+
+module.exports = {
+    AuthenticationError: new GraphQLError('Error authenticating user.', {
+        extensions: {
+            code: 'UNAUTHENTICATED',
+        },
+    }),
+
+    authMiddleware: function ({ req }) {
+        let token = req.body.token || req.query.token || req.headers.authorization;
+
+        if (req.headers.authorization) {
+            token = token.split(' ').pop().trim();
+        }
+
+        if (!token) {
+            return req;
+        }
+
+        try {
+            const { data } = jwt.verify(token, secret, { maxAge: expiration });
+            req.user = data;
+        } catch {
+            console.log('Invalid token.');
+        }
+
+        return req;
+    },
+    signToken: function ({ email }) {
+        const payload = { email };
+
+        return jwt.sign({ data: payload }, secret, { expiresIn: expiration });
+    },
+};
