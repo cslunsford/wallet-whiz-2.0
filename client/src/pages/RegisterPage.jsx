247884973f03df0a29a2de46e8344feeedebe962
JObengDappah <134343671+JObengDappah@users.noreply.github.com>
2023-11-07 18:54:33 -0500

    Register page converted to React and added

---
+++ client/src/pages/RegisterPage.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css'; // Ensure Bootstrap CSS is imported
+import './RegisterPage.css'; // Import the dedicated CSS for this component
+
+const RegisterPage = () => {
+  // Implement form submission logic here, if necessary
+
+  return (
+    <div className="container">
+      <div className="row justify-content-center">
+        <div className="col-md-8 col-lg-6">
+          <div className="registerPage">
+            <h3 className="tagline">Welcome to the Future of Finance</h3>
+            <h4 className="taglineSubtext mb-4">Take control of your finances now</h4>
+            <div className="formBoxHeader">Register</div>
+            <form>
+              <div className="form-floating mb-3">
+                <input type="email" className="form-control" id="registerEmail" name="email" placeholder="name@example.com" />
+                <label htmlFor="registerEmail">Email Address</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input type="password" className="form-control" id="registerPassword" name="password" placeholder="Password" />
+                <label htmlFor="registerPassword">Password</label>
+              </div>
+              <div className="form-floating mb-3">
+                <input type="password" className="form-control" id="repeatPassword" name="passwordConfirm" placeholder="Password" />
+                <label htmlFor="repeatPassword">Confirm Password</label>
+              </div>
+              <button type="submit" id="register" className="btn btn-primary btn-lg btn-block">Register</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RegisterPage;
