db2e871d4c0cfe31692bd37f7d7c65abe824705d
Chaz Coats <chazcoats2@hotmail.comgit config --global user.email chazcoats2@hotmail.com>
2023-11-07 20:45:00 -0500

    Nav bar set up. Might need slight tweaks

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './Navbar.css';
+import Logo from '../assets/walletWhiz logo2.png';
 
 const Navbar = () => {
     return (
         <nav class="navbar">
         <div class="header">
             <h1 class="headerText">WalletWhiz</h1>
-            <img class="headerLogo" src="../assets/WalletWiz logo.png"></img>
+            <img class="headerLogo" src={Logo}></img>
         </div>
         <ul class="nav-links">
             <li><a class="listText" href="#">Dashboard</a></li>
