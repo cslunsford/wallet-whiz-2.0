3c4562a00e5c72dd17e4ea98f72dfb96bc36096b
Chaz Coats <chazcoats2@hotmail.comgit config --global user.email chazcoats2@hotmail.com>
2023-11-07 21:21:05 -0500

    reconnected stylesheet to main.jsx

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css'; 
 import '../App.css'; 
-import Chart from 'react-chartjs-2'; 
+//import Chart from 'react-chartjs-2'; 
 
 const Dashboard = () => {
   // Placeholder data
