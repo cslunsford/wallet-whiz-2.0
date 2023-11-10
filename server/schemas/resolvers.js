21ffd8d65bb23d8b7b1bfe1b0714a8925eb8dad1
cslunsford <christopher.lunsford@uconn.edu>
2023-11-09 19:11:55 -0500

    added mutations for logging in and registering

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -24,5 +24,31 @@ const resolvers = {
                 throw AuthenticationError;
             }
         }
+    },
+    Mutation: {
+        login: async (parent, { email, password }) => {
+            const user = await User.findOne({ email });
+
+            if (!user) {
+                throw AuthenticationError;
+            }
+
+            const correctPw = await user.isCorrectPassword(password);
+
+            if (!correctPw) {
+                throw AuthenticationError;
+            }
+
+            const token = signToken(user);
+            return { token, user };
+        },
+        register: async (parent, { email, password }) => {
+            const user = await User.create({ email, password });
+            const token = signToken(user);
+
+            return { token, user };
+        }
     }
 }
+
+module.exports = resolvers;
