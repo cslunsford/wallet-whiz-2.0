b452988f373475fe72f752651ce41d18fb2e7a2c
Chaz Coats <chazcoats2@hotmail.com>
2023-11-14 11:24:48 -0500

    fixing register button functionality

--- client/src/pages/homepage.jsx
+++ client/src/pages/homepage.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Button from '@mui/material/Button';
 import LoginForm from '../components/LoginForm';
+import { Link } from 'react-router-dom';
 
 function homepage() {
     return (
@@ -24,7 +25,9 @@ function homepage() {
                         </div>
                         <div className="registerButtonBox">
                             <h3 className="registerHeader">Not yet managing your money? Register here</h3>
+                            <Link to='/register'>
                             <Button variant="contained" disableElevation>Register</Button>
+                            </Link>
                         </div>
                     </div>
                 </div>
