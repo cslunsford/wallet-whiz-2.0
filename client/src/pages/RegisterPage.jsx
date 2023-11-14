da752043814c13051c29c9f1a2d0846ae53ff6e3
Chaz Coats <chazcoats2@hotmail.comgit config --global user.email chazcoats2@hotmail.com>
2023-11-13 19:57:25 -0500

    fixing styling issues and page theme

--- client/src/pages/RegisterPage.jsx
+++ client/src/pages/RegisterPage.jsx
@@ -4,16 +4,14 @@ import RegisterForm from '../components/RegisterForm';
 const RegisterPage = () => {
 
   return (
-    <div className="container">
-      <div className="row justify-content-center">
-        <div className="col-md-8 col-lg-6">
+    <div className="container upperContainer">
+        <div className="container">
           <div className="registerPage">
             <h3 className="tagline">Welcome to the Future of Finance</h3>
             <h4 className="taglineSubtext mb-4">Take control of your finances now</h4>
             <div className="formBoxHeader">Register</div>
             <RegisterForm />
           </div>
-        </div>
       </div>
     </div>
   );
--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -9,8 +9,9 @@ function User() {
     const [userId, setUserId] = useState(null);
 
     return (
-        <div className='userpage'>
-            <div className="container-fluid registerPage">
+        <div className="container upperContainer">
+        <div className='container'>
+            <div className="container registerPage">
                 <h3 className="tagline">User Profile</h3>
                 <div className="displayCards">
                     <h3 className="cardHeader">Account Information</h3>
@@ -52,34 +53,8 @@ function User() {
                     </div>
                     {<PlaidButton userId={userId} />}
                 </div>
-                <div className="usertextbottom">
-                    <div className="container-fluid appFeaturesBox">
-                        <h2 className="featuresHeader">
-                            What does WalletWhiz have to offer you?
-                        </h2>
-                        <div className="container-fluid featuresBoxInner">
-                            <div className="container-fluid featuresTextBox">
-                                <h3 className="featuresText">
-                                    WalletWhiz tracks your transactions in real time so that you can
-                                    accurately manage incoming and outgoing finances
-                                </h3>
-                            </div>
-                            <div className="container-fluid featuresTextBox">
-                                <h3 className="featuresText">
-                                    WalletWhiz has tools to analyze your spending habits so you can
-                                    save more money for the things you want
-                                </h3>
-                            </div>
-                            <div className="container-fluid featuresTextBox">
-                                <h3 className="featuresText">
-                                    WalletWhiz is made by people like you, for people like you. We
-                                    implemented features that we want to track our own finances
-                                </h3>
-                            </div>
-                        </div>
-                    </div>
-                </div>
             </div>
+        </div>
         </div>
     );
 };
