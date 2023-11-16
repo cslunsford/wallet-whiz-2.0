e8bc0fff9e55345dc04148f83c29353c07324a6f
cslunsford <christopher.lunsford@uconn.edu>
2023-11-16 12:28:50 -0500

    preparing for deployment

--- server/routes/plaidRoutes.js
+++ server/routes/plaidRoutes.js
@@ -10,7 +10,7 @@ router.post('/create_link_token', async function (req, res) {
         client_name: 'Plaid Test App',
         products: ['auth'],
         language: 'en',
-        redirect_uri: 'http://localhost:3001/api',
+        redirect_uri: process.env.REDIRECT_URI || 'http://localhost:3001/api',
         country_codes: ['US'],
     };
     console.log(plaidRequest);
